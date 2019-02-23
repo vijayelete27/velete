8495b314c2ef5b3af984caef1b9498d081d33c83
velete <reddyeleti@gmail.com>
2019-02-23 08:55:51 +0800

    Demo Java Class

---
+++ example/src/com/demo/test/example/DemoClass.java
@@ -0,0 +1,10 @@
+package com.demo.test.example;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sample test demo");
+	}
+
+}
